ab9cac90cd013467a9bd8d5fe36232381dda6882
Eric Daniels <eric@erdaniels.com>
2018-11-03 14:04:55 -0400

    Starting sync code

---
+++ app/src/main/java/com/mongodb/stitch_car_test/Rover.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2018-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.stitch_car_test;
+
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonObjectId;
+import org.bson.BsonReader;
+import org.bson.BsonString;
+import org.bson.BsonValue;
+import org.bson.BsonWriter;
+import org.bson.Document;
+import org.bson.codecs.BsonDocumentCodec;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Rover {
+
+  public static final String ROVERS_DATABASE = "rover";
+  public static final String ROVERS_COLLECTION = "rovers";
+
+  private final String id;
+  private final ObjectId lastMoveCompleted;
+  private final List<Document> moves;
+
+  Rover(
+      final String id,
+      final ObjectId lastMoveCompleted,
+      final List<Document> moves
+  ) {
+    this.id = id;
+    this.lastMoveCompleted = lastMoveCompleted;
+    this.moves = moves;
+  }
+
+  Rover(final String userId) {
+    this(userId, null, Collections.emptyList());
+  }
+
+  public Rover(final Rover rover, List<Document> moves) {
+    this(rover.getId(), rover.getLastMoveCompleted(), moves);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public ObjectId getLastMoveCompleted() {
+    return lastMoveCompleted;
+  }
+
+  public List<Document> getMoves() {
+    return moves;
+  }
+
+  static BsonDocument toBsonDocument(final Rover rover) {
+    final BsonDocument asDoc = new BsonDocument();
+    asDoc.put(Fields.ID, new BsonString(rover.getId()));
+    if (rover.getLastMoveCompleted() != null) {
+      asDoc.put(Fields.LAST_MOVE_COMPLETED, new BsonObjectId(rover.getLastMoveCompleted()));
+    }
+    final BsonArray movesArr = new BsonArray();
+    for (final Document move : rover.getMoves()) {
+      movesArr.add(BsonDocument.parse(move.toJson()));
+    }
+    asDoc.put(Fields.MOVES, movesArr);
+    return asDoc;
+  }
+
+  static Rover fromBsonDocument(final BsonDocument doc) {
+    final ObjectId lastMoveCompleted;
+    if (doc.containsKey(Fields.LAST_MOVE_COMPLETED)) {
+      lastMoveCompleted = doc.getObjectId(Fields.LAST_MOVE_COMPLETED).getValue();
+    } else {
+      lastMoveCompleted = null;
+    }
+    final List<Document> moves;
+    if (doc.containsKey(Fields.MOVES)) {
+      final BsonArray movesArr = doc.getArray(Fields.MOVES);
+      moves = new ArrayList<>(movesArr.size());
+      for (final BsonValue moveVal : movesArr) {
+        moves.add(Document.parse(moveVal.asDocument().toJson()));
+      }
+    } else {
+      moves = Collections.emptyList();
+    }
+    return new Rover(
+        doc.getString(Fields.ID).getValue(),
+        lastMoveCompleted,
+        moves
+    );
+  }
+
+  static final class Fields {
+    static final String ID = "_id";
+    static final String LAST_MOVE_COMPLETED = "lastMoveCompleted";
+    static final String MOVES = "moves";
+  }
+
+  public static final Codec<Rover> codec = new Codec<Rover>() {
+
+    @Override
+    public void encode(
+        final BsonWriter writer, final Rover value, final EncoderContext encoderContext) {
+      new BsonDocumentCodec().encode(writer, toBsonDocument(value), encoderContext);
+    }
+
+    @Override
+    public Class<Rover> getEncoderClass() {
+      return Rover.class;
+    }
+
+    @Override
+    public Rover decode(
+        final BsonReader reader, final DecoderContext decoderContext) {
+      final BsonDocument document = (new BsonDocumentCodec()).decode(reader, decoderContext);
+      return fromBsonDocument(document);
+    }
+  };
+}
---
+++ app/src/main/java/com/mongodb/stitch_car_test/RoverActivity.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2018-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mongodb.stitch_car_test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+// Base Stitch Packages
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.core.auth.providers.serverapikey.ServerApiKeyCredential;
+import com.mongodb.stitch.core.internal.common.BsonUtils;
+
+// Packages needed to interact with MongoDB and Stitch
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.ConflictHandler;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.internal.ChangeEvent;
+
+//General Document Classes
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RoverActivity extends Activity implements ConflictHandler<Rover> {
+    private static final String TAG = "RoverActivity";
+
+    private RemoteMongoCollection<Rover> rovers;
+    private String userId;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        final StitchAppClient client = Stitch.getDefaultAppClient();
+        final RemoteMongoClient mongoClient =
+            client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+        rovers = mongoClient.getDatabase(Rover.ROVERS_DATABASE)
+            .getCollection(Rover.ROVERS_COLLECTION, Rover.class)
+            .withCodecRegistry(CodecRegistries.fromRegistries(
+                BsonUtils.DEFAULT_CODEC_REGISTRY,
+                CodecRegistries.fromCodecs(Rover.codec)));
+
+        rovers.sync().configure(
+            this,
+            null,
+            (documentId, error) -> Log.e(TAG, error.getLocalizedMessage()));
+
+        doLogin();
+    }
+
+    private void doLogin() {
+        Stitch.getDefaultAppClient().getAuth().loginWithCredential(
+            new ServerApiKeyCredential(getString(R.string.stitch_rover_api_key)))
+            .addOnSuccessListener(user -> {
+                userId = user.getId();
+                invalidateOptionsMenu();
+                Toast.makeText(RoverActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+
+                if (rovers.sync().getSyncedIds().isEmpty()) {
+                    rovers.sync().insertOne(new Rover(userId));
+                }
+
+                moveLoop();
+            })
+            .addOnFailureListener(e -> {
+                invalidateOptionsMenu();
+                Log.d(TAG, "error logging in", e);
+                Toast.makeText(RoverActivity.this, "Failed logging in", Toast.LENGTH_SHORT).show();
+            });
+    }
+
+    private Document getRoverFilter() {
+        return new Document("_id", userId);
+    }
+
+    private Document getLatestMoveFilter() {
+        return getRoverFilter()
+            .append("moves",
+                new Document("$exists", true)
+                    .append("$not", new Document("$size", 0)));
+    }
+
+    private void moveLoop() {
+        rovers.sync().find(getLatestMoveFilter()).first().addOnSuccessListener(rover -> {
+            if (rover == null) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                moveLoop();
+            } else {
+                final Document move = rover.getMoves().get(0);
+                doMove(move);
+                final Document update = new Document("$pull", new Document("moves",
+                    new Document("_id", move.getObjectId("_id"))));
+                rovers.sync().updateOne(getRoverFilter(), update).addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.d(TAG, "failed to update rover document", task.getException());
+                    }
+                    moveLoop();
+                });
+            }
+        }).addOnFailureListener(e -> Log.d(TAG, "failed to find rover document", e));
+    }
+
+    private void doMove(final Document move) {
+        Log.i(TAG, "Doing move " + move.toJson());
+    }
+
+    @Override
+    public Rover resolveConflict(
+        final BsonValue documentId,
+        final ChangeEvent<Rover> localEvent,
+        final ChangeEvent<Rover> remoteEvent
+    ) {
+        if (localEvent.getFullDocument().getLastMoveCompleted() == null) {
+            return remoteEvent.getFullDocument();
+        }
+        // Given this sync model consists of a single producer and a single consumer, a conflict
+        // can only occur when a production and consumption happens at the same "time". That means
+        // that there should always be an overlap of moves during a conflict and that the last
+        // move completed is always present in the remote. Therefore we should trim all moves up to
+        // and including the last completed move.
+        final Rover localRover = localEvent.getFullDocument();
+        final ObjectId lastMoveCompleted = localRover.getLastMoveCompleted();
+        final Rover remoteRover = remoteEvent.getFullDocument();
+        final List<Document> nextMoves = new ArrayList<>(remoteRover.getMoves().size());
+        boolean caughtUp = false;
+        for (final Document move : remoteRover.getMoves()) {
+            if (move.getObjectId("_id").equals(lastMoveCompleted)) {
+                caughtUp = true;
+            }
+            if (caughtUp) {
+                nextMoves.add(move);
+            }
+        }
+        return new Rover(localRover, nextMoves);
+    }
+}
